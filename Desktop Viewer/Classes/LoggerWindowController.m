6f2a6f2177291bf996788cad45859504c8f442e1
Paul Capestany <capestany@gmail.com>
2013-06-25 17:45:56 -0700

    Double-click behavior and multiple windows fixes
    Instead of opening the "detail view", double-clicking on a log item now immediately takes you straight to the line in the source code where the log statement was made. Using double-click + alt opens the "detail view" instead, since this is a less-likely scenario.
    
    Also, tweaked the AppleScript to make it faster (the call to `open "$file"` was *especially* affecting responsiveness). In case there are multiple NSLogger debugging sessions going on, as well as multiple Xcode project windows open, the changes to xedReplacement.sh bring the proper window to be the frontmost while still avoiding the slowness of the `open` command in the bash script.

--- Desktop Viewer/Classes/LoggerWindowController.m
+++ Desktop Viewer/Classes/LoggerWindowController.m
@@ -809,11 +809,12 @@ static NSArray *sXcodeFileExtensions = nil;
 	[detailsWindowController showWindow:self];
 }
 
-- (void)xedFile:(NSString *)path line:(NSString *)line { 
+- (void)xedFile:(NSString *)path line:(NSString *)line client:(NSString *)client {
     id args = [NSArray arrayWithObjects:
                @"-l",
                line,
                path,
+               client,
                nil];
     // NSLog(@"Args %@", args);
     [NSTask launchedTaskWithLaunchPath:[[NSBundle mainBundle] pathForResource:@"xedReplacement.sh" ofType:nil]
@@ -822,11 +823,16 @@ static NSArray *sXcodeFileExtensions = nil;
 
 - (void)logCellDoubleClicked:(id)sender
 {
-	// Added in v1.1: alt-double click opens the source file if it was defined in the log
-	// and the file is found
+	// double click opens the source file if it was defined in the log
+	// and the file is found (using alt can mess with the results of the AppleScript)
+    // alt + double click opens the detail view
 	NSEvent *event = [NSApp currentEvent];
-	if ([event clickCount] > 1 && ([NSEvent modifierFlags] & NSAlternateKeyMask) != 0)
-	{
+    if ([event clickCount] > 1 && ([NSEvent modifierFlags] & NSAlternateKeyMask) != 0)
+    {
+        [self openDetailsWindow:sender];
+    }
+    else if ([event clickCount] > 1)
+    {
 		NSInteger row = [logTable selectedRow];
 		if (row >= 0 && row < [displayedMessages count])
 		{
@@ -857,7 +863,8 @@ static NSArray *sXcodeFileExtensions = nil;
 					if (useXcode)
 					{                        
                         [self xedFile:filename 
-                                 line:[NSString stringWithFormat:@"%d", MAX(0, msg.lineNumber) + 1]];
+                                 line:[NSString stringWithFormat:@"%d", MAX(0, msg.lineNumber)]
+                               client:[attachedConnection clientName]];
 					}
 					else
 					{
@@ -868,7 +875,6 @@ static NSArray *sXcodeFileExtensions = nil;
 		}
 		return;
 	}
-	[self openDetailsWindow:sender];
 }
 
 // -----------------------------------------------------------------------------
