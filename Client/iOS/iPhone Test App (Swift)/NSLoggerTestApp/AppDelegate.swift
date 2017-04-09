439fcb16486afd82a3dca7204802360b34dcc86b
Florent Pillet <fpillet@gmail.com>
2017-04-09 17:18:31 +0200

    Make sure NSLogger is found on the network

--- Client/iOS/iPhone Test App (Swift)/NSLoggerTestApp/AppDelegate.swift
+++ Client/iOS/iPhone Test App (Swift)/NSLoggerTestApp/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         // Avoid NSLogger client to search on Bonjour.
-        LoggerSetViewerHost(nil, "localhost" as NSString, 50000)
+        //LoggerSetViewerHost(nil, "localhost" as NSString, 50000)
 
         Log(.App, .Important, "Hello, Swift Logger Tester! 🤖")
 
