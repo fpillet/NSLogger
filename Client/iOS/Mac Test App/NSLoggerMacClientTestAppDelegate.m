0c74796fbfcd8e4543bd2893471bf26b5f22bc14
Florent Pillet <fpillet@gmail.com>
2017-04-13 23:23:45 +0200

    Tweaked logs from mac test app

--- Client/iOS/Mac Test App/NSLoggerMacClientTestAppDelegate.m
+++ Client/iOS/Mac Test App/NSLoggerMacClientTestAppDelegate.m
@@ -103,16 +103,16 @@
 			[s appendFormat:@"%c", 32 + (arc4random() % 27)];
 		int what = (arc4random() % 5);
 		if (what == 0)
-			LogMessage([tagsArray objectAtIndex:(arc4random() % [tagsArray count])], arc4random() % 3, s);
+			LogMessage([tagsArray objectAtIndex:(arc4random() % [tagsArray count])], arc4random() % 3, @"%@", s);
 		else if (what == 1)
 		{
 			// log full origin info
-			LogMessageF(__FILE__, __LINE__, __FUNCTION__, [tagsArray objectAtIndex:(arc4random() % [tagsArray count])], arc4random() % 3, s);
+			LogMessageF(__FILE__, __LINE__, __FUNCTION__, [tagsArray objectAtIndex:(arc4random() % [tagsArray count])], arc4random() % 3, @"%@", s);
 		}
 		else if (what == 2)
 		{
 			// just log __FUNCTION__
-			LogMessageF(NULL, 0, __FUNCTION__, [tagsArray objectAtIndex:(arc4random() % [tagsArray count])], arc4random() % 3, s);
+			LogMessageF(NULL, 0, __FUNCTION__, [tagsArray objectAtIndex:(arc4random() % [tagsArray count])], arc4random() % 3, @"%@", s);
 		}
 		else if (what == 3)
 		{
@@ -121,7 +121,7 @@
 		else
 		{
 			// just log __FILE__ __LINE__
-			LogMessageF(__FILE__, __LINE__, NULL, [tagsArray objectAtIndex:(arc4random() % [tagsArray count])], arc4random() % 3, s);
+			LogMessageF(__FILE__, __LINE__, NULL, [tagsArray objectAtIndex:(arc4random() % [tagsArray count])], arc4random() % 3, @"%@", s);
 		}
 	}
 	else if (phase == 1)
@@ -181,7 +181,7 @@
 
 - (void)sendLogFromAnotherThread:(NSNumber *)counterNum
 {
-	LogMessage(@"transfers", 0, @"message %d from standalone thread", [counterNum integerValue]);
+	LogMessage(@"transfers", 0, @"message %d from standalone thread", (int)[counterNum integerValue]);
 }
 
 @end
